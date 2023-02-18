b17b55459af114082ab9cfa49463354607f93940
Midnight <23142346+Midnight145@users.noreply.github.com>
2023-02-17 18:33:13 -0600

    couple more checks

--- src/main/java/midnight/filefix/EnterChunkEventHandler.java
+++ src/main/java/midnight/filefix/EnterChunkEventHandler.java
@@ -6,6 +6,7 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.fybertech.modelcitizens.TileEntityCorpse;
 import net.minecraft.entity.item.EntityMinecartChest;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -19,15 +20,15 @@ import net.minecraftforge.event.entity.EntityEvent;
 import net.minecraftforge.event.entity.EntityEvent.EnteringChunk;
 
 public class EnterChunkEventHandler {
-	
+
 	public EnterChunkEventHandler() {
 		MinecraftForge.EVENT_BUS.register(this);
 		System.out.println("I am being created");
 	}
-
+	
 	@SubscribeEvent
 	public void onEnterChunk(EntityEvent.EnteringChunk event) {
-		if (event.entity.worldObj.isRemote) { return; }
+		if (event.entity.worldObj.isRemote || !(event.entity instanceof EntityPlayerMP)) { return; }
 		final World world = event.entity.worldObj;
 		final FileFixWorldSavedData data = FileFixWorldSavedData.get(world);
 		if (!data.broadswordFixed) {
@@ -37,9 +38,9 @@ public class EnterChunkEventHandler {
 			this.doCaysykaBookFix(event, world, data);
 		}
 	}
-	
-	private void doCaysykaBookFix(EnteringChunk event, World world, FileFixWorldSavedData data) {
 
+	private void doCaysykaBookFix(EnteringChunk event, World world, FileFixWorldSavedData data) {
+		
 		if (event.newChunkX == -67 && event.newChunkZ == -64) {
 			System.out.println("Fixing caysyka book");
 			if (world.provider.dimensionId != 0) { return; }
@@ -49,6 +50,9 @@ public class EnterChunkEventHandler {
 			NBTTagCompound tag = null;
 			if (model == null) {
 				System.out.println("Unable to find model");
+				data.caysykaBookFixed = true;
+				data.markDirty();
+				return;
 			}
 			try {
 				tag = (NBTTagCompound) JsonToNBT.func_150315_a(nbtString);
@@ -56,16 +60,18 @@ public class EnterChunkEventHandler {
 			catch (final NBTException e) {
 				e.printStackTrace();
 				System.out.println("Error converting string to NBT");
+				data.caysykaBookFixed = true;
+				data.markDirty();
 				return;
 			}
 			if (tag == null) { return; }
 			itemstack.readFromNBT(tag);
-			model.setInventorySlotContents(1, itemstack);
+			model.setInventorySlotContents(1, itemstack);	
 			data.caysykaBookFixed = true;
 			data.markDirty();
 		}
 	}
-	
+
 	public void doSwordFix(EntityEvent.EnteringChunk event, World world, FileFixWorldSavedData data) {
 		if (event.newChunkX == 102 && event.newChunkZ == 7) {
 			System.out.println("Fixing broadsword");
@@ -76,34 +82,40 @@ public class EnterChunkEventHandler {
 			for (final EntityMinecartChest chest : chests) {
 				System.out.println(chest.toString());
 			}
-
+			
 			for (final EntityMinecartChest chest : chests) {
 				if (chest.getStackInSlot(13) == null) {
 					final String nbtString = "{id:4502,Count:1,Damage:0,tag:{InfiTool:{BaseDurability:405,Head:101,Tooltip1:\"§dBeheading\",ToolEXP:0l,Effect1:13,HarvestLevel:4,ModifierTip1:\"§dBeheading\",Attack:9,RenderHead:101,ModDurability:0.0f,Handle:14,Broken:0b,Shoddy:0.0f,RenderHandle:14,Accessory:123,MiningSpeed:500,RenderAccessory:123,ToolLevel:1,Unbreaking:2,Damage:0,Beheading:1,BonusDurability:0,Modifiers:0,TotalDurability:405},display:{Name:\"§fArtaxerxes\"}}}";
-
+					
 					final Item item = GameRegistry.findItem("TConstruct", "broadsword");
 					if (item == null) {
 						System.out.println("Failed to create item!");
+						data.caysykaBookFixed = true;
+						data.markDirty();
 					}
 					final ItemStack itemstack = new ItemStack(item, 1, 0);
 					NBTTagCompound tag = null;
-
+					
 					try {
 						tag = (NBTTagCompound) JsonToNBT.func_150315_a(nbtString);
 					}
 					catch (final NBTException e) {
 						System.out.println("Error converting string to NBT");
+						data.caysykaBookFixed = true;
+						data.markDirty();
 						return;
 					}
 					if (tag == null) { return; }
 					itemstack.readFromNBT(tag);
 					chest.setInventorySlotContents(13, itemstack);
-					data.broadswordFixed = true; // set broadswordFixed to true
-					data.markDirty();
+					
 				}
+				data.broadswordFixed = true; // set broadswordFixed to true
+				data.markDirty();
+				return;
 			}
 		}
-		
-	}
 
+	}
+	
 }
